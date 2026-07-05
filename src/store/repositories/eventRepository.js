5b5090d2920c4c554acc035a3f62789a6b8165af
divya9658 <23mh1a05m4@acoe.edu.in>
2026-07-05 13:10:32 +0530

    Added another function called getStats to get the statistics of severity.

--- src/store/repositories/eventRepository.js
+++ src/store/repositories/eventRepository.js
@@ -28,20 +28,43 @@ function saveEvent(event) {
     );
 }
 
-function getEvents(callback) {
-    db.all(
-        `
+function getEvents(filters, callback) {
+    let query = `
         SELECT *
         FROM events
-        ORDER BY timestamp DESC
-        `,
-        [],
-        (err, rows) => {
-            callback(err, rows);
-        }
-    );
-}
+    `;
+
+    let values = [];
+
+    if (filters.animal) {
+        query += " WHERE animal = ?";
+        values.push(filters.animal);
+    }
+
+    if (filters.severity) {
+        query += filters.animal
+            ? " AND severity = ?"
+            : " WHERE severity = ?";
+
+        values.push(filters.severity);
+    }
+
+    query += " ORDER BY timestamp DESC";
+
+    db.all(query, values, (err, rows) => {
+        callback(err, rows);
+    });
+};
+
+function getStats(callback) {
+    const query = `SELECT severity,COUNT(*) AS count FROM events GROUP BY severity;`
+    db.all(query, [], (err, rows) => {
+        callback(err, rows);
+    });
+};
 module.exports = {
     saveEvent,
-    getEvents
+    getEvents,
+    getStats
 };
+        
